18a5d1299a2af785035b5881cfaa5a51262d3d33
Day-Fan <56620175+Rakaze14@users.noreply.github.com>
2025-10-01 14:34:38 +0900

    Update webcontrol.ino
    added reset to initial position function

--- webcontrol.ino
+++ webcontrol.ino
@@ -2,9 +2,9 @@
 #include <ESPAsyncWebServer.h>
 #include <LittleFS.h>
 #include <ESP32Servo.h>
+
 #define BASE_PIN 18
 #define SHOULDER_PIN 19
-
 
 // ====== Wi-Fi Config ======
 const char* ssid = "YOUR_WIFI_SSID";
@@ -17,14 +17,14 @@ AsyncWebServer server(80);
 Servo baseServo;
 Servo shoulderServo;
 
-int basePos = 90;    
+int basePos = 90;
 int shoulderPos = 90;
 
 void setup() {
   Serial.begin(115200);
 
   // Mount LittleFS
-  if(!LittleFS.begin(true)){
+  if (!LittleFS.begin(true)) {
     Serial.println("LittleFS Mount Failed!");
     return;
   }
@@ -45,23 +45,47 @@ void setup() {
   Serial.println("\nWiFi connected. IP: " + WiFi.localIP().toString());
 
   // Serve HTML page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(LittleFS, "/index.html", "text/html");
   });
 
   // Handle slider requests
-  server.on("/set", HTTP_GET, [](AsyncWebServerRequest *request){
-    if(request->hasParam("base")){
+  server.on("/set", HTTP_GET, [](AsyncWebServerRequest *request) {
+    if (request->hasParam("base")) {
       basePos = request->getParam("base")->value().toInt();
       baseServo.write(basePos);
       Serial.printf("Base: %d\n", basePos);
     }
-    if(request->hasParam("shoulder")){
+    if (request->hasParam("shoulder")) {
       shoulderPos = request->getParam("shoulder")->value().toInt();
       shoulderServo.write(shoulderPos);
       Serial.printf("Shoulder: %d\n", shoulderPos);
     }
     request->send(200, "text/plain", "OK");
+  });
+
+  // Handle reset request (smooth simultaneous movement)
+  server.on("/reset", HTTP_GET, [](AsyncWebServerRequest *request) {
+    int targetBase = 90;
+    int targetShoulder = 90;
+    int stepDelay = 15; // ms between steps
+
+    // Move until both servos reach target
+    while (basePos != targetBase || shoulderPos != targetShoulder) {
+      if (basePos < targetBase) basePos++;
+      else if (basePos > targetBase) basePos--;
+
+      if (shoulderPos < targetShoulder) shoulderPos++;
+      else if (shoulderPos > targetShoulder) shoulderPos--;
+
+      baseServo.write(basePos);
+      shoulderServo.write(shoulderPos);
+
+      delay(stepDelay);
+    }
+
+    Serial.println("Arm back to initial position");
+    request->send(200, "text/plain", "RESET DONE");
   });
 
   server.begin();
